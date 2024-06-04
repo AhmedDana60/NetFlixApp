7d85359c7c51336ef54ff501c5bc30302389fc3c
AhmedDana60 <ahmeddanamo@gmail.com>
2024-06-04 13:42:38 +0300

    Initial Commit

---
+++ NetFlixApp/NetFlixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetFlixAppApp.swift
+//  NetFlixApp
+//
+//  Created by Ahmed Dana Mohammed on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NetFlixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
